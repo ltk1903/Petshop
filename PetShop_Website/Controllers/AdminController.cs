f308184c067bbe99ac6219be71f7464ec1685dea
ltk1903 <khangltk19032002@gmail.com>
2025-03-21 16:01:15 +0700

    first commit

---
+++ PetShop_Website/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PetShop_Website.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
