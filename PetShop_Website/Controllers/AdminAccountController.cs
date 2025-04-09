a9b8072abde79dfeabde25dd2713899702c4f80c
ltk1903 <khangltk19032002@gmail.com>
2025-04-09 18:02:00 +0700

    create components admin page

---
+++ PetShop_Website/Controllers/AdminAccountController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PetShop_Website.Controllers
+{
+    public class AdminAccountController : Controller
+    {
+        // GET: AdminAccount
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
