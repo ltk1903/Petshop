65c2aadf41f0bffa7e5bc769d1a6ec1e379d8092
ltk1903 <khangltk19032002@gmail.com>
2025-04-10 16:06:35 +0700

    connect database & build add product feature

---
+++ PetShop_Website/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetShop_Website.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Role { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+        public virtual ICollection<Reviewer> Reviewers { get; set; }
+    }
+}
