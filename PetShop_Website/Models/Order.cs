d45f57131e00f3e417d9ede4ae59227294d081f7
ltk1903 <khangltk19032002@gmail.com>
2025-04-10 23:31:03 +0700

    fix model

--- PetShop_Website/Models/Order.cs
+++ PetShop_Website/Models/Order.cs
@@ -8,6 +8,8 @@ namespace PetShop_Website.Models
     public class Order
     {
         public int OrderID { get; set; }
+
+        // Khóa ngoại đến User
         public int UserID { get; set; }
         public virtual User User { get; set; }
 
@@ -15,6 +17,7 @@ namespace PetShop_Website.Models
         public string OrderStatus { get; set; }
         public DateTime OrderDate { get; set; }
 
+        // Quan hệ với chi tiết đơn hàng
         public virtual ICollection<OrderDetail> OrderDetails { get; set; }
         public virtual Payment Payment { get; set; }
     }
--- PetShop_Website/Models/PetShopContext.cs
+++ PetShop_Website/Models/PetShopContext.cs
@@ -12,7 +12,11 @@ namespace PetShop_Website.Models
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
-
+        public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Reviewer> Reviewers { get; set; }
 
     }
 }
